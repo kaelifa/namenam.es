85adf7bbcee45829b66d54e3cc62e672d0871df5
Adam Auckland <adamauckland@gmail.com>
2013-09-08 16:21:53 +0100

    adding functionality

--- django-server/web/namenames/frontend/models.py
+++ django-server/web/namenames/frontend/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+import django.db.models
+from django.contrib.auth.models import User, Group, UserManager
 
 # Create your models here.
+
+class SuggestedName(django.db.models.Model):
+    name = django.db.models.TextField(max_length=255)
+    user = django.db.models.ForeignKey(User)
+
+
+class UserToUser(django.db.models.Model):
+    user = django.db.models.ForeignKey(User)
+    linked_user = django.db.models.ForeignKey(User, related_name='linked_user')
